c54a1afd9af76e1cebe2c2e3f2748d06dd56fdbc
Unknown <niels.bierman@hva.nl>
2020-03-17 22:43:39 +0100

    huts

--- index.js
+++ index.js
@@ -9,31 +9,71 @@ const PREFIX = '!';
 
 const deurSound = new Sound('deur.mp3');
 
+let usedBelrecently = false;
+
+let usedPikofbalRecently = new Set();
+
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.on('message', msg=>{
+    //console.log(msg.emoji.id);
     if(msg.content === "nice"){
-        msg.react('🤔')
-    }
+        msg.react(':ok_hand:')
+            }
     let args = msg.content.substring(PREFIX.length).split(" ");
 
-
     switch(args[0]){
+
         case 'pikofbal':
-            msg.channel.send((Math.random() > 0.5)? "pik" : "bal");
+            if(usedPikofbalRecently.has(msg.author.id)){
+                msg.reply("Doe rustig a woeshoem, kut Ivar");
+            }else {
+                msg.channel.send((Math.random() > 0.5)? "pik" : "bal");
+                usedPikofbalRecently.add(msg.author.id);
+                setTimeout(() =>{
+                    usedPikofbalRecently.delete(msg.author.id);
+                }, 3000)
+            }
             break;
+
         case 'bel':
-            const role = msg.guild.roles.fetch('689137029912592385');
-            let connection = msg.member.voice.channel.join();
-            console.log(role);
-           // msg.member.addRole(role.id);
-            let player = connection.playStream(deurSound);
-            msg.channel.send("Deur", {
-                tts: true
-            });
+            if(usedBelrecently === true){
+                msg.reply("Doe rustig a woeshoem we gooien shantan die deur open")
+            }else {
+                msg.channel.send("DING DONG", {
+                    tts: true
+                });
+                msg.member.roles.add('689137029912592385');
+
+                usedBelrecently = true;
+                setTimeout(() =>{
+                    usedBelrecently = false;
+                }, 50000)
+            }
             break;
+
+        case 'test':
+
+            // msg.delete();
+            // bot.users.fetch().then((bigher) => {
+            //     console.log(bigher);
+            // }).catch((error) =>{
+            //     console.log(error);
+            // });
+            // break;
+
+        // case 'bel69':
+        //     const role = msg.guild.roles.fetch('689137029912592385');
+        //     let connection = msg.member.voice.channel.join();
+        //     console.log(role);
+        //    // msg.member.addRole(role.id);
+        //     let player = connection.playStream(deurSound);
+        //     msg.channel.send("Deur", {
+        //         tts: true
+        //     });
+        //     break;
     }
 });
 
